43106366c71309d3b17a3de1c2a05953c3e643fd
pyk <2213646+pyk@users.noreply.github.com>
2025-05-30 09:59:36 +0700

    scope: add files

---
+++ src/validator-registry/avs/MevCommitAVSStorage.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: BSL 1.1
+pragma solidity 0.8.26;
+
+import { IMevCommitAVS } from "../../interfaces/IMevCommitAVS.sol";
+
+import { IAVSDirectory } from "../../eigen//IAVSDirectory.sol";
+import { IDelegationManager } from "../../eigen//IDelegationManager.sol";
+import { IEigenPodManager } from "../../eigen//IEigenPodManager.sol";
+import { IStrategyManager } from "../../eigen//IStrategyManager.sol";
+
+abstract contract MevCommitAVSStorage {
+    /// @notice reference to eigenlayer core delegation manager contract
+    IDelegationManager internal _delegationManager;
+
+    /// @notice reference to eigenlayer core eigenpod manager contract
+    IEigenPodManager internal _eigenPodManager;
+
+    /// @notice reference to eigenlayer core strategy manager contract
+    IStrategyManager internal _strategyManager;
+
+    /// @notice reference to eigenlayer core AVS directory contract
+    IAVSDirectory internal _eigenAVSDirectory;
+
+    /// @notice Mapping of operator addresses to their registration info
+    mapping(address => IMevCommitAVS.OperatorRegistrationInfo) public operatorRegistrations;
+
+    /// @notice Mapping of validator pubkeys to their registration info
+    mapping(bytes => IMevCommitAVS.ValidatorRegistrationInfo) public validatorRegistrations;
+
+    /// @notice Mapping of LST restaker address to their registration info
+    mapping(address => IMevCommitAVS.LSTRestakerRegistrationInfo) public lstRestakerRegistrations;
+
+    /// @notice List of restakeable strategy addresses
+    address[] public restakeableStrategies;
+
+    /// @notice Address of the oracle responsible for freezing validators.
+    address public freezeOracle;
+
+    /// @notice Fee required to unfreeze a validator.
+    uint256 public unfreezeFee;
+
+    /// @notice Address that will receive unfreeze fees from frozen validators.
+    address public unfreezeReceiver;
+
+    /**
+     * @notice Number of blocks a validator must remain frozen before it can be unfrozen.
+     * This is param is optional to allow frozen validators to pay the fee immediately.
+     */
+    uint256 public unfreezePeriodBlocks;
+
+    /// @notice Number of blocks an operator must wait after requesting deregistration before it is finalized.
+    uint256 public operatorDeregPeriodBlocks;
+
+    /// @notice Number of blocks a validator must wait after requesting deregistration before it is finalized.
+    uint256 public validatorDeregPeriodBlocks;
+
+    /// @notice Number of blocks a LST restaker must wait after requesting deregistration before it is finalized.
+    uint256 public lstRestakerDeregPeriodBlocks;
+
+    /// @dev See https://docs.openzeppelin.com/upgrades-plugins/1.x/writing-upgradeable#storage-gaps
+    uint256[48] private __gap;
+}
